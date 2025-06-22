0194aef3db0ace4119889ac70b990c919abd7678
BishalBaral20033 <bishalbaral20033@gmail.com>
2025-06-22 20:40:49 +0530

    Add files via upload

---
+++ Week 1/Algorithms_Data Structures/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class FinancialForecast
+{
+    // Recursive method to calculate future value
+    static double PredictFutureValueRecursive(double initialValue, double growthRate, int years)
+    {
+        if (years == 0)
+            return initialValue;
+
+        return PredictFutureValueRecursive(initialValue, growthRate, years - 1) * (1 + growthRate);
+    }
+
+    // Iterative method to calculate future value
+    static double PredictFutureValueIterative(double initialValue, double growthRate, int years)
+    {
+        double value = initialValue;
+        for (int i = 0; i < years; i++)
+        {
+            value *= (1 + growthRate);
+        }
+        return value;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter initial investment amount: ");
+        double initialValue = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter annual growth rate (in percent, e.g., 10 for 10%): ");
+        double growthPercent = Convert.ToDouble(Console.ReadLine());
+        double growthRate = growthPercent / 100.0;
+
+        Console.Write("Enter number of years to forecast: ");
+        int years = Convert.ToInt32(Console.ReadLine());
+
+        double futureRecursive = PredictFutureValueRecursive(initialValue, growthRate, years);
+        double futureIterative = PredictFutureValueIterative(initialValue, growthRate, years);
+
+        Console.WriteLine($"\nForecasting result after {years} years:");
+        Console.WriteLine($"Recursive prediction: {futureRecursive:F2}");
+        Console.WriteLine($"Iterative prediction: {futureIterative:F2}");
+    }
+}
